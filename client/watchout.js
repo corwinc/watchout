5dee77810352a4c2b7f383771af661ea962adfc4
corwinc <corwin.crownover@gmail.com>
2016-08-05 15:24:39 -0700

    Add the board, library Underscore, and define global settings

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,12 @@
 // start slingin' some d3 here.
+
+var gameSettings = {
+  height: 450,
+  width: 700,
+  nEnemies: 30
+};
+
+var gameBoard = d3.select('.board')
+  .append('svg:svg')
+    .attr('width', gameSettings.width)
+    .attr('height', gameSettings.height);
