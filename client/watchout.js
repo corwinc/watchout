125740a8a4acb08112df18ec241de59d57a00906
corwinc <corwin.crownover@gmail.com>
2016-08-05 20:26:49 -0700

    Add user to gameboard and move enemies

--- client/watchout.js
+++ client/watchout.js
@@ -8,26 +8,114 @@ var gameSettings = {
   r: 10
 };
 
+var userSettings = [{
+  x: 225,
+  y: 350,
+  r: 5
+}];
+
+// var drag = d3.behavior.drag()
+//             .on('drag', function(d, i) {
+//               d.x += d3.event.dx;
+//               d.y += d3.event.dy;
+//               d3.select(this).attr('transform', function(d, i) {
+//                 return 'translate(' + [ d.x, d.y ] + ')';
+//               });
+//             });
+
 // Define the game board
 var gameBoard = d3.select('.board')
   .append('svg')
     .attr('width', gameSettings.width)
     .attr('height', gameSettings.height);
 
-// Generate n enemies
-var enemies = _.range(0, gameSettings.nEnemies).map(function(id) {
-  return {
-    'id': id,
-    'x': Math.random() * gameSettings.height,
-    'y': Math.random() * gameSettings.width
-  };
-});
-
-gameBoard.selectAll('.enemy')
-  .data(enemies, function(d) { return d.id; })
+gameBoard.selectAll('.player')
+  .data(userSettings)
   .enter().append('circle')
-    .attr('cx', function(d, i) { return d.x; })
-    .attr('cy', function(d, i) { return d.y; })
-    .attr('r', gameSettings.r)
-    .attr('class', 'enemy')
-    .attr('fill', 'aqua');
+    .attr('class', 'user')
+    .attr('fill', 'red')
+    .attr('r', function(d) { return d.r; })
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .on('mouseover', function () {
+      console.log(this);
+      d3.select(this).style('fill', 'blue');
+    });
+    // .attr('transform', 'translate(' + x + ',' + y + ')')
+    // .call(drag);
+
+var update = function() {
+  // Generate n enemies
+  enemyData = _.range(0, gameSettings.nEnemies).map(function(id) {
+    return {
+      'id': id,
+      'x': Math.random() * gameSettings.width,
+      'y': Math.random() * gameSettings.height
+    };
+  });
+ 
+  enemies = gameBoard.selectAll('.enemy')
+    .data(enemyData, function(d, i) { return d.id; });
+    
+
+  enemies.enter().append('circle')
+      .attr('class', 'enemy')
+      .attr('fill', 'aqua')
+      .attr('r', 0)
+      //.transition().duration(1000)
+      .attr('cx', function(d, i) { return d.x; })
+      .attr('cy', function(d, i) { return d.y; });
+
+  enemies
+      .data(enemyData)
+      .attr('r', gameSettings.r)
+      //.transition().duration(1000)
+      .attr('cx', function(d, i) { return d.x; })
+      .attr('cy', function(d, i) { return d.y; });
+
+      
+  //enemies.exit().remove();
+  /*gameBoard.selectAll('.enemy')
+    .transition().duration(1000)
+    .attr('cx', Math.random() * gameSettings.width)
+    .attr('cy', Math.random() * gameSettings.height);*/
+      
+
+};
+
+update();
+setInterval(function() {
+  update();
+}, 1000);
+
+var drag = d3.behavior.drag()
+            .on('drag', function(d, i) {
+              d.x += d3.event.dx;
+              d.y += d3.event.dy;
+              d3.select(this).attr('transform', function(d, i) {
+                return 'translate(' + [ d.x, d.y ] + ')';
+              });
+            });
+
+
+
+// d3.select('.player').call(d3.drag().on('mousedown'));
+
+//update();
+//d3.interval(update(), 1000);
+/*enemies.enter()
+  .data()
+  .transition().duration(1000)
+    .attr('cx', function(data) { return d.x + 10; });
+*/
+// gameBoard.selectAll('.enemy')
+//     .transition().duration(1000)
+//     .attr('cx', function(d, i) { return d3.randomUniform(100)(); } )
+//     .attr('cy', function(d, i) { return d3.randomUniform(100)(); } );
+
+/*_.range(0, gameSettings.nEnemies).map(function(id) {
+  return gameBoard.select(id)
+    .transition().duration(1000)
+    .attr('cx', function(d, i) { return d3.randomUniform(100)(); } )
+    .attr('cy', function(d, i) { return d3.randomUniform(100)(); } );
+});*/
