ceaffb4693ec94e7a9fbac38a6d8d162360a2d9b
corwinc <corwin.crownover@gmail.com>
2016-08-05 17:14:49 -0700

    Add enemies to board

--- client/watchout.js
+++ client/watchout.js
@@ -1,12 +1,33 @@
 // start slingin' some d3 here.
 
+// Settings
 var gameSettings = {
   height: 450,
   width: 700,
-  nEnemies: 30
+  nEnemies: 30,
+  r: 10
 };
 
+// Define the game board
 var gameBoard = d3.select('.board')
-  .append('svg:svg')
+  .append('svg')
     .attr('width', gameSettings.width)
     .attr('height', gameSettings.height);
+
+// Generate n enemies
+var enemies = _.range(0, gameSettings.nEnemies).map(function(id) {
+  return {
+    'id': id,
+    'x': Math.random() * gameSettings.height,
+    'y': Math.random() * gameSettings.width
+  };
+});
+
+gameBoard.selectAll('.enemy')
+  .data(enemies, function(d) { return d.id; })
+  .enter().append('circle')
+    .attr('cx', function(d, i) { return d.x; })
+    .attr('cy', function(d, i) { return d.y; })
+    .attr('r', gameSettings.r)
+    .attr('class', 'enemy')
+    .attr('fill', 'aqua');
